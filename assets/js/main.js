40e3a97c3da86cbe0ede63864db765eb4ab4acf0
Rafael Santana <santana_r@outlook.com>
2025-05-09 13:37:53 -0300

    Adiciona função de redirecionamento para adicionar efeito de animação

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,3 +4,22 @@ const hora = document.querySelector('.hora');
 const data = document.querySelector('.data');
 
 buscarDataHoraPersonalizada(hora, data);
+
+
+
+function animarERedirecionar(e) {
+  e.preventDefault();
+  document.body.classList.add("animacao-saida");
+
+  setTimeout(() => {
+    window.location.href = e.target;
+  }, 900); 
+}
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const link = document.getElementById('link-login');
+    if(link) {
+        link.addEventListener('click', animarERedirecionar);
+    }
+})
