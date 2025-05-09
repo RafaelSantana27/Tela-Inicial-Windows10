c83c65b7dfe70a6c840d1402af68dea03babdb9d
Rafael Santana <santana_r@outlook.com>
2025-05-09 14:49:33 -0300

    Corrigido variaveis e criado evento de retorno a formulario login, zerado e dando foco a campo input

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,19 +1,27 @@
 
-
+const formLogin = document.querySelector('.login-input');
+const formLoginErro = document.querySelector('.login-input-erro');
+const inputSenha = document.getElementById('login');
 
 const btn = document.getElementById('btn');
+const btnErro = document.getElementById('btnErro');
+
 
 btn.addEventListener('click', function(){
-
-  const login = document.getElementById('login').value;
   
-  if(login === 'abc123') {
-    console.log('Senha está correta');
-    window.location.href = 'pagina_Inicial.html';
+  if(inputSenha.value === 'abc123') {
+      console.log('Senha está correta');
+      window.location.href = 'pagina_Inicial.html';
   }else {
     console.log("A senha está incorreta tente novamente");
-    // botão de ok para!
+    formLogin.style.display = "none";
+    formLoginErro.style.display = "block";
+    }
+})
 
-  }
-
+btnErro.addEventListener('click', function(){
+    formLoginErro.style.display = "none";
+    formLogin.style.display = "flex";
+    inputSenha.value = '';
+    inputSenha.focus();
 })
